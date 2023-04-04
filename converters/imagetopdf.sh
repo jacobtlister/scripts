e1956b2cab6cfebdad8da65782b49da5ee709ad2
jacobtlister <jacoblister52262@gmail.com>
2023-04-04 17:45:03 -0400

    updated based on shellcheck recomendations

--- converters/imagetopdf.sh
+++ converters/imagetopdf.sh
@@ -9,11 +9,13 @@ required packages
 additional notes
     to be able to use this program, you have to make sure ghostscript has permission to create / edit pdfs
     if the program returns the following error,
-        attempt to perform an operation not allowed by the security policy `PDF'
+        attempt to perform an operation not allowed by the security policy 'PDF'
 
     use the following guide to fix it (need sudo to edit the policy.xml file)
         https://suay.site/?p=2369
 notes
+
+# code starts here --------------------------------------------------------------------------------
 
 # array of most common image file extensions I see in daily / common use
 imageExtensions=("png" "jpg" "jpeg" "jfif" "svg" "webp")
@@ -26,26 +28,28 @@ function validExtension {
 
     # while i < imageExtensions.length
     while [ ${i} -lt ${#imageExtensions[@]} ]; do
-        if [ "${extension}" = "${imageExtensions[${i}]}" ]; then
+        if [ "$extension" = "${imageExtensions[${i}]}" ]; then
             return 1
         fi
 
-        let i=$i+1
+        (( i++ )) || true
     done
 
     return 0
 }
 
-for filename in $(ls); do
-    extension=${filename##*.} # gets everything after last . (normally the whole file extension)
+for file in *.*; do
+    [[ -e "$file" ]] || break  # handle the case of no files
+
+    extension=${file##*.} # gets everything after last '.' (normally the whole file extension)
     extension=${extension,,}  # converts the entire string to lower case (for easier comparisons)
 
     validExtension
 
     # if exit code of validExtension = 1, perform the conversion
     if [ $? == 1 ]; then
-        convert "${filename}" "${filename%.*}.pdf"
-        rm -f "${filename}"
+        convert "$file" "${file%.*}.pdf"
+        rm -f "$file"
     fi
 
 done
--- converters/videotoav1.sh
+++ converters/videotoav1.sh
@@ -7,9 +7,13 @@
         ffmpeg (apt)
 
     additional notes
-        additional notes go here
+        referenced the following wikipedia page for the array of file extensions,
+            https://en.wikipedia.org/wiki/Video_file_format
 notes
 
+# code starts here --------------------------------------------------------------------------------
+
+# array which contains many types of video file extensions
 videoExtensions=("3g2" "3gp" "amv" "asf" "avi" "drc" "flv" "f4v" "f4p" "f4a" "f4b" "m4v" "mng" "mov" "qt" "mp4" "m4p" "m4v" "mpg" "mpg2" "mpeg" "mpe" "mpv" "m2v" "mts" "m2ts" "ts" "mxf" "nsv" "ogv" "ogg" "rm" "rmvb" "roq" "svi" "viv" "vob" "webm" "wmv" "yuv")
 
 # Checks if the passed in string matches to one of the entries in image extensions
@@ -19,27 +23,27 @@ function validExtension {
     local i=0
 
     # while i < imageExtensions.length
-    while [ ${i} -lt ${#imageExtensions[@]} ]; do
-        if [ "${extension}" = "${imageExtensions[${i}]}" ]; then
+    while [ ${i} -lt ${#videoExtensions[@]} ]; do
+        if [ "$extension" = "${videoExtensions[${i}]}" ]; then
             return 1
         fi
 
-        let i=$i+1
+        (( i++ )) || true
     done
 
     return 0
 }
 
-for filename in $(ls); do
-    extension=${filename##*.} # gets everything after last . (normally the whole file extension)
+for file in *.*; do
+    extension=${file##*.} # gets everything after last '.' (normally the whole file extension)
     extension=${extension,,}  # converts the entire string to lower case (for easier comparisons)
 
     validExtension
 
     # if exit code of validExtension = 1, perform the conversion
     if [ $? == 1 ]; then
-        ffmpeg -i "${filename}" -c:v libaom-av1 "${filename%.*}.mkv"
-        rm -f "${filename}"
+        ffmpeg -i "$file" -c:v libaom-av1 "${file%.*}.mkv"
+        rm -f "$file"
     fi
 
 done

54e16c6ce6baba57090a0d521b124c720a3c5956
jacobtlister <jacoblister52262@gmail.com>
2026-03-15 08:26:41 +1200

    added support for 1 argument and removed an extraneous echo

--- staples/pdfweave.sh
+++ staples/pdfweave.sh
@@ -31,12 +31,25 @@
         
         then front-weave.pdf will be ordered as follows:
             front1, back1, front2, back2, front3, back3, front4, front5, front6
+        
+        there is also the option to run the script with 1 argument, as follows:
+            bash pdfweave.sh filename_no_extension
+        
+        in the case above, this is equivalent to the following:
+            bash pdfweave.sh "${filename_no_extension}.pdf" "${filename_no_extension}b.pdf"
+        
+        for example, if we run pdfweave.sh with the following argument:
+            bash pdfweave.sh file1
+        
+        it will be equivalent to the following:
+            bash pdfweave.sh file1.pdf file1b.pdf
 info
 
-echo ${file1}
+if [ $# == 1 ]; then
+    pdftk A="${1}.pdf" B="${1}b.pdf" shuffle A B output "${1}-weave.pdf"
 
-if [ $# == 2 ]; then
-    pdftk A="${1}" B="${2}" shuffle A B output "${1%.*}"-weave.pdf
+elif [ $# == 2 ]; then
+    pdftk A="${1}" B="${2}" shuffle A B output "${1%.*}-weave.pdf"
 
 elif [ $# == 3 ]; then
     pdftk ${1} cat 1-$((${3} - 1)) output temp1.pdf
@@ -44,7 +57,7 @@ elif [ $# == 3 ]; then
     
     pdftk A=temp2.pdf B=${2} shuffle A B output temp3.pdf
     
-    pdftk A=temp1.pdf B=temp3.pdf cat A B output "${1%.*}"-weave.pdf
+    pdftk A=temp1.pdf B=temp3.pdf cat A B output "${1%.*}-weave.pdf"
     
     rm temp1.pdf temp2.pdf temp3.pdf
 

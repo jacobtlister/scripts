21d34d8be43026266d78bf4535a48d672865780e
jacobtlister <jacoblister52262@gmail.com>
2026-03-31 18:00:11 +1200

    some minor changes to the templates

--- templates/ftemplate.sh
+++ templates/ftemplate.sh
@@ -11,7 +11,7 @@ info
 
 # because this lives in /funcs/, cannot just source all in
 # scripts/funcs/, else recursion. instead, just source what's needed
-# commenting this so ShellCheck doesn't freak out
+# commenting this so shellcheck doesn't freak out
 # shellcheck source=/dev/null
 # source "${SCRIPTS_PATH}/funcs/func.sh"
 

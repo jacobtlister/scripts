420815fb89609635e6084e3775ef08dda0d883a0
jacobtlister <jacoblister@protonmail.com>
2026-09-01 18:53:02 +1200

    added LATEX_PROJECT_PATH and JATEXSTUDIO environment variables to .jaliases

--- setup/setupjaliases.sh
+++ setup/setupjaliases.sh
@@ -25,34 +25,48 @@ if [ "$SCRIPTS_PATH" == "" ]; then
     echo "updating file database..."
     sudo updatedb
 
-    # get path to scripts repository
-    path="$(locate /scripts/setup/setup | head -n 1)"
-    path="${path%/setup/*}"
+    # get path to each repository
+    path1="$(locate /scripts/setup/setup | head -n 1)"
+    path1="${path1%/setup/*}"
+    path2="$(locate /latex-projects/jatex | head -n 1)"
+    path2="${path2%/setup/*}"
+    path3="$(locate /jatexstudio/reup | head -n 1)"
+    path3="${path3%/setup/*}"
 
-    echo "scripts repository path is: ${path}"
+    echo "scripts        repository path is: ${path1}"
+    echo "latex-projects repository path is: ${path2}"
+    echo "jatexstudio    repository path is: ${path3}"
 
     # inserts \ before any / in path for use in a sed command
-    sedpath="${path//\//\\/}"
+    sedpath1="${path1//\//\\/}"
+    sedpath2="${path2//\//\\/}"
+    sedpath3="${path3//\//\\/}"
 
     # place .jaliases in ~/
     # update ~/.jaliases if it already exists
-    cp "${path}/setup/.jaliases_template" ~/.jaliases
-    sed -i "5s/\/path\/to\/scripts/${sedpath}/" ~/.jaliases
+    cp "${path1}/setup/.jaliases_template" ~/.jaliases
+    sed -i "5s/\/path\/to\/scripts/${sedpath1}/" ~/.jaliases
+    sed -i "6s/\/path\/to\/latex-projects/${sedpath2}/" ~/.jaliases
+    sed -i "7s/\/path\/to\/jatexstudio/${sedpath3}/" ~/.jaliases
 
     # place .shellcheckrc in ~/
-    cp "${path}/setup/.shellcheckrc_template" ~/.shellcheckrc
+    cp "${path1}/setup/.shellcheckrc_template" ~/.shellcheckrc
 
 # if ${SCRIPTS_PATH} is defined (ie .jaliases exists or existed some point recently)
 # just use the pre-existing ${SCRIPTS_PATH} value instead of updating the database
 # and searching for the path to the scripts repo
 else
     # inserts \ before any / in path for use in a sed command
-    sedpath="${SCRIPTS_PATH//\//\\/}"
+    sedpath1="${SCRIPTS_PATH//\//\\/}"
+    sedpath2="${LATEX_PROJECTS_PATH//\//\\/}"
+    sedpath3="${JATEXSTUDIO_PATH//\//\\/}"
 
     # make a copy of .jaliases in ~/
     # update ~/.jaliases if it already exists
     cp "${SCRIPTS_PATH}/setup/.jaliases_template" ~/.jaliases
-    sed -i "5s/\/path\/to\/scripts/${sedpath}/" ~/.jaliases
+    sed -i "5s/\/path\/to\/scripts/${sedpath1}/" ~/.jaliases
+    sed -i "6s/\/path\/to\/latex-projects/${sedpath2}/" ~/.jaliases
+    sed -i "7s/\/path\/to\/jatexstudio/${sedpath3}/" ~/.jaliases
 
     # place .shellcheckrc in ~/
     cp "${SCRIPTS_PATH}/setup/.shellcheckrc_template" ~/.shellcheckrc
@@ -64,7 +78,10 @@ grep "\. ~/.jaliases" ~/.bashrc > /dev/null
 
 # source .jaliases in ~/.bashrc if its not already
 if [ $? == 1 ]; then
-    cat "${path}/setup/bashrc_jaliases_append.txt" >> ~/.bashrc
+    path1="$(locate /scripts/setup/setup | head -n 1)"
+    path1="${path1%/setup/*}"
+
+    cat "${path1}/setup/bashrc_jaliases_append.txt" >> ~/.bashrc
     echo "placed .jaliases source statement in ~/.bashrc"
     echo "placed .jaliases in ~/"
     echo "placed .shellcheckrc in ~/"
